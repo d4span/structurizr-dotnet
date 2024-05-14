973331737bd19463f8cd346dba96a07093ec0914
David Caspar <david.caspar@aol.ch>
2024-05-15 01:01:46 +0200

    WIP

--- Structurizr.Core.New/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Structurizr.Core.New
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ Structurizr.Core.New/Model.fs
@@ -0,0 +1,13 @@
+﻿namespace Structurizr.Core.New
+
+open FSharp.Collections
+open Structurizr
+
+type Model =
+    { ImpliedRelationshipsStrategy: IImpliedRelationshipsStrategy
+      IdGenerator: IdGenerator
+      Enterprise: Enterprise
+      People: Set<Person>
+      SoftwareSystems: Set<SoftwareSystem> 
+      DeploymentNodes: Set<DeploymentNode> 
+      Relationships: Relationship list }
